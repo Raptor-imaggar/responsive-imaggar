a8526ca1a72bedc31ce43c237c49c70285badc45
Raptor-imaggar <131775100+Raptor-imaggar@users.noreply.github.com>
2023-11-27 10:13:48 -0800

    header responsive

--- src/data/menu.js
+++ src/data/menu.js
@@ -1,3 +1,5 @@
+import personImage from './person.png';
+
 const headerMenu = [
   {
     name: { en: "Home", tamil: "முகப்பு" },
@@ -6,31 +8,51 @@ const headerMenu = [
   {
     name: { en: "About", tamil: "எங்களை பற்றி" },
     url: "/about",
-    submenu : [{ name : {en:"sub1",tamil:"தயாரி"},url : "/sub1"},{ name : {en:"sub1",tamil:"தயாரி"},url : "/sub1"},{ name : {en:"sub1",tamil:"தயாரி"},url : "/sub1"}]
-
+    submenu: [
+      { name: { en: "Why Imaggar", tamil: "தயாரி" }, url: "/sub1", image: personImage },
+      { name: { en: "Partnership", tamil: "தயாரி" }, url: "/sub2", image: personImage },
+      { name: { en: "About Us", tamil: "தயாரி" }, url: "/sub3", image: personImage },
+    ],
   },
   {
     name: { en: "Contacts", tamil: "தொடர்பு கொள்ள" },
     url: "/contact",
-    submenu : [{ name : {en:"sub1",tamil:"தயாரி"},url : "/sub1"},{ name : {en:"sub1",tamil:"தயாரி"},url : "/sub1"},{ name : {en:"sub1",tamil:"தயாரி"},url : "/sub1"}]
-
+    submenu: [
+      { name: { en: "contact1", tamil: "தயாரி" }, url: "/sub1", image: personImage },
+      { name: { en: "Call", tamil: "தயாரி" }, url: "/sub2", image: personImage },
+      { name: { en: "Bot", tamil: "தயாரி" }, url: "/sub3", image: personImage },
+    ],
   },
   {
     name: { en: "Careers", tamil: "பணியாளர் வேலைகள்" },
     url: "/careers",
-    submenu : [{ name : {en:"sub1",tamil:"தயாரி"},url : "/sub1"},{ name : {en:"sub1",tamil:"தயாரி"},url : "/sub1"},{ name : {en:"sub1",tamil:"தயாரி"},url : "/sub1"}]
-
+    submenu: [
+      { name: { en: "Intership", tamil: "தயாரி" }, url: "/sub1", image: personImage },
+      { name: { en: "Job", tamil: "தயாரி" }, url: "/sub2", image: personImage },
+      
+    ],
   },
   {
     name: { en: "Services", tamil: "சேவைகள்" },
     url: "/services",
-    submenu : [{ name : {en:"sub1",tamil:"தயாரி"},url : "/sub1"},{ name : {en:"sub1",tamil:"தயாரி"},url : "/sub1"},{ name : {en:"sub1",tamil:"தயாரி"},url : "/sub1"}]
-
+    submenu: [
+      { name: { en: "Maas", tamil: "தயாரி" }, url: "/sub1", image: personImage},
+      { name: { en: "Baas", tamil: "தயாரி" }, url: "/sub2", image: personImage},
+      { name: { en: "SDaas", tamil: "தயாரி" }, url: "/sub3",image: personImage },
+      { name: { en: "VMaas", tamil: "தயாரி" }, url: "/sub2",image: personImage },
+    
+    ],
   },
   {
     name: { en: "Products", tamil: "தயாரிப்புகள்" },
     url: "/products",
-    submenu : [{ name : {en:"sub1",tamil:"தயாரி"},url : "/sub1"},{ name : {en:"sub1",tamil:"தயாரி"},url : "/sub1"},{ name : {en:"sub1",tamil:"தயாரி"},url : "/sub1"}]
+    submenu: [
+      { name: { en: "SITE", tamil: "தயாரி" }, url: "/sub1", image: personImage},
+      { name: { en: "CORE", tamil: "தயாரி" }, url: "/sub2", image: personImage},
+      { name: { en: "TRADE", tamil: "தயாரி" }, url: "/sub3",image: personImage },
+      { name: { en: "VOICE", tamil: "தயாரி" }, url: "/sub2",image: personImage },
+      { name: { en: "CARE", tamil: "தயாரி" }, url: "/sub2", image: personImage},
+    ],
   },
 ];
 
--- src/src/desktop/components/others/Header/index.js
+++ src/src/desktop/components/others/Header/index.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Header.css';
 import headerMenu from '../../../../../data/menu';
 
 const Header = () => {
   const [currentNav, setCurrentNav] = useState(null);
   const [navsVisited, setNavsVisited] = useState({});
+  const [isScrolled, setIsScrolled] = useState(false);
 
   const handleMouseEnter = (e, item) => {
     setCurrentNav(item.name.en);
@@ -19,11 +20,26 @@ const Header = () => {
     setCurrentNav(null);
   };
 
+  const handleScroll = () => {
+    // Set isScrolled to true when the user has scrolled down
+    setIsScrolled(window.scrollY > 0);
+  };
+
+  useEffect(() => {
+    // Add scroll event listener when the component mounts
+    window.addEventListener('scroll', handleScroll);
+
+    // Remove the scroll event listener when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="header">
+    <header className={`header ${isScrolled ? 'scrolled' : ''}`}>
       {/* Logo */}
       <div className="logo">
-        <img src='./logo.png' />
+        <img src='./logo.png' alt="Logo" />
       </div>
 
       {/* Navigation */}
@@ -40,11 +56,18 @@ const Header = () => {
               {item.name.en}
               {item.submenu && currentNav === item.name.en && (
                 <ul className={`submenu ${currentNav === item.name.en ? 'active' : ''}`}>
-                  {item.submenu.map((subItem, subIndex) => (
-                    <li key={subIndex}>
-                      <a href={subItem.url}>{subItem.name.en}</a>
-                    </li>
-                  ))}
+                  <div className="submenu-list">
+                    {item.submenu.map((subItem, subIndex) => (
+                      <div key={subIndex} className="submenu-item-container">
+                        <li>
+                          <a href={subItem.url}>
+                            <img src={subItem.image} alt={subItem.name.en} className="submenu-icon" />
+                            {subItem.name.en}
+                          </a>
+                        </li>
+                      </div>
+                    ))}
+                  </div>
                 </ul>
               )}
             </li>
