d41221436c066aee0834885033ac62ccef6d1998
Raptor-imaggar <131775100+Raptor-imaggar@users.noreply.github.com>
2023-11-28 11:30:17 -0800

    contact page  added

---
+++ src/data/dataContact.js
@@ -0,0 +1,39 @@
+// dataContact.js
+
+export const contactData = {
+    mainTitle: "Generative AI-Powered Support to Win Lifelong Customers",
+    subheading: "With Haptik, you can:",
+    listItems: [
+      {
+        icon: "AiOutlineCheckCircle",
+        heading: "Build a Generative AI Assistant in No Time!",
+        description: "Empower agents with Gen AI-powered chat suggestions, intent detection & other contextual information for effective query resolution.",
+      },
+      {
+        icon: "AiOutlineCheckCircle",
+        heading: "Drive Better Customer Outcomes with Analytics",
+        description: "Empower agents with Gen AI-powered chat suggestions, intent detection & other contextual information for effective query resolution.",
+      },
+      {
+        icon: "AiOutlineCheckCircle",
+        heading: "Unlock Agent Efficiency with Agent Co-pilot",
+        description: "Empower agents with Gen AI-powered chat suggestions, intent detection & other contextual information for effective query resolution.",
+      },
+      {
+        icon: "AiOutlineCheckCircle",
+        heading: "Drive Better Customer Outcomes with Analytics",
+        description: "Empower agents with Gen AI-powered chat suggestions, intent detection & other contextual information for effective query resolution.",
+      },
+    ],
+    demoRequestTitle: "Request a Demo",
+    formLabels: {
+      firstName: "First Name",
+      lastName: "Last Name",
+      companyName: "Company Name",
+      email: "Email",
+      phoneNumber: "Phone Number",
+      selectPlaceholder: "Select",
+      submitButton: "Submit",
+    },
+  };
+  
--- src/src/desktop/components/contact/index.js
+++ src/src/desktop/components/contact/index.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import Select from 'react-select';
 import './Contact.css';
 import { AiOutlineCheckCircle } from 'react-icons/ai';
+import { contactData } from '../../../../data/dataContact';
 
 const ContactPage = () => {
   const [countryCodes, setCountryCodes] = useState([]);
@@ -30,103 +31,107 @@ const ContactPage = () => {
 
     fetchData();
   }, []);
-    return (
-        <div className="contact-main">
-            <div className="contact-container">
-                <div className="contact-content">
-                    <h2>
-                        <span className="highlight-text">Generative AI-Powered</span> Support to Win Lifelong Customers
-                    </h2>
 
-                    <h4>With Haptik, you can:</h4>
-                    <ul className='contact-ul-tag'>
-                    <li className="list-item">
-                            <div className="bullet-point">
-                                <AiOutlineCheckCircle className="bullet-icon" />
-                            </div>
-                            <div className="list-content">
-                                <span className="heading">Build a Generative AI Assistant in No Time!</span> <br />
-                                <span className="description">Empower agents with Gen AI-powered chat suggestions, intent detection & other contextual information for effective query resolution.</span>
-                            </div>
-                        </li>
-                        <li className="list-item">
-                            <div className="bullet-point">
-                                <AiOutlineCheckCircle className="bullet-icon" />
-                            </div>
-                            <div className="list-content">
-                                <span className="heading">Drive Better Customer Outcomes with Analytics</span> <br />
-                                <span className="description">Empower agents with Gen AI-powered chat suggestions, intent detection & other contextual information for effective query resolution.</span>
-                            </div>
-                        </li>
-                        <li className="list-item">
-                            <div className="bullet-point">
-                                <AiOutlineCheckCircle className="bullet-icon" />
-                            </div>
-                            <div className="list-content">
-                                <span className="heading">Unlock Agent Efficiency with Agent Co-pilot</span> <br />
-                                <span className="description">Empower agents with Gen AI-powered chat suggestions, intent detection & other contextual information for effective query resolution.</span>
-                            </div>
-                        </li>
-                        <li className="list-item">
-                            <div className="bullet-point">
-                                <AiOutlineCheckCircle className="bullet-icon" />
-                            </div>
-                            <div className="list-content">
-                                <span className="heading">Drive Better Customer Outcomes with Analytics</span> <br />
-                                <span className="description">Empower agents with Gen AI-powered chat suggestions, intent detection & other contextual information for effective query resolution.</span>
-                            </div>
-                        </li>
-                        
-                    </ul>
-                    <div className="logo-row">
-                        {/* Add your logo images here */}
-                    </div>
-                </div>
-                <div className="contact-form">
-                    <h2>Request a Demo</h2>
-                    <form>
-                    <div className="form-group">
-  <div className="form-row">
-    <input className='Nameinput FirstName' placeholder='First Name' type="text" id="firstName" name="firstName" />
-    <div className="space-between"></div> {/* Add a spacer between the inputs */}
-    <input className='Nameinput LastName' placeholder='Last Name' type="text" id="lastName" name="lastName" />
-  </div>
-</div>
+  return (
+    <div className="contact-main">
+      <div className="contact-container">
+        <div className="contact-content">
+          <h2>
+            <span className="highlight-text">{contactData.mainTitle}</span>
+          </h2>
 
-                        <div className="form-group">
-                            <input className='contact-input' placeholder='Company Name' type="text" id="companyName" name="companyName" />
-                        </div>
-                        <div className="form-group">
-                            <input className='contact-input' placeholder='Company Name' type="text" id="companyName" name="companyName" />
-                        </div>
-                        <div className="form-group">
-                            <input className='contact-input' placeholder='Email' type="email" id="email" name="email" />
-                        </div>
-                        <div className="form-group">
-                            <div className="form-row">
-                            <Select
-              className="country-select contact-input"
-              options={countryCodes}
-              value={selectedCountry}
-              onChange={(value) => setSelectedCountry(value)}
-              placeholder="Select"
-              menuPlacement="auto" // This makes the dropdown open at the top or bottom based on available space
-            />
-                                <input
-                                    className="contact-input phone-input"
-                                    placeholder="Phone Number"
-                                    type="text"
-                                    id="phoneNumber"
-                                    name="phoneNumber"
-                                />
-                            </div>
-                        </div>
-                        <button className='contact-submit-button' type="submit">Submit</button>
-                    </form>
+          <h4>{contactData.subheading}</h4>
+          <ul className="contact-ul-tag">
+            {contactData.listItems.map((item, index) => (
+              <li key={index} className="list-item">
+                <div className="bullet-point">
+                  <AiOutlineCheckCircle className="bullet-icon" />
                 </div>
-            </div>
+                <div className="list-content">
+                  <span className="heading">{item.heading}</span> <br />
+                  <span className="description">{item.description}</span>
+                </div>
+              </li>
+            ))}
+          </ul>
+          <div className="logo-row">{/* Add your logo images here */}</div>
         </div>
-    );
+        <div className="contact-form">
+          <h2>{contactData.demoRequestTitle}</h2>
+          <form>
+            <div className="form-group">
+              <div className="form-row">
+                <input
+                  className="Nameinput FirstName"
+                  placeholder={contactData.formLabels.firstName}
+                  type="text"
+                  id="firstName"
+                  name="firstName"
+                />
+                <div className="space-between"></div>
+                <input
+                  className="Nameinput LastName"
+                  placeholder={contactData.formLabels.lastName}
+                  type="text"
+                  id="lastName"
+                  name="lastName"
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <input
+                className="contact-input"
+                placeholder={contactData.formLabels.companyName}
+                type="text"
+                id="companyName"
+                name="companyName"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="contact-input"
+                placeholder={contactData.formLabels.companyName}
+                type="text"
+                id="companyName"
+                name="companyName"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="contact-input"
+                placeholder={contactData.formLabels.email}
+                type="email"
+                id="email"
+                name="email"
+              />
+            </div>
+            <div className="form-group">
+              <div className="form-row">
+                <Select
+                  className="country-select contact-input"
+                  options={countryCodes}
+                  value={selectedCountry}
+                  onChange={(value) => setSelectedCountry(value)}
+                  placeholder={contactData.formLabels.selectPlaceholder}
+                  menuPlacement="auto"
+                />
+                <input
+                  className="contact-input phone-input"
+                  placeholder={contactData.formLabels.phoneNumber}
+                  type="text"
+                  id="phoneNumber"
+                  name="phoneNumber"
+                />
+              </div>
+            </div>
+            <button className="contact-submit-button" type="submit">
+              {contactData.formLabels.submitButton}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
