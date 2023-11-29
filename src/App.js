eb89b13ceed29d088dfa260c0da46f817094e69a
thowfick-official <mdthowfickspeed@gmail.com>
2023-11-29 09:33:45 -0800

    mobile contact page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 // App.js
 
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './src/mobile';
 import DesktopRoutes from './src/desktop';
 import CustomCursor from './CustomCursor';
 import Loading from './src/LoadingSpinner/LoadingSpinner';
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(true); // State variable to control loading screen
+  const [isLoading, setIsLoading] = useState(true);
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [isMobile, setIsMobile] = useState(
+    window.matchMedia('(max-width: 767px)').matches ||
+      window.location.search.includes('forceMobileView')
+  );
 
   const onMouseMove = (e) => {
     const { clientX, clientY } = e;
@@ -18,35 +22,32 @@ const App = () => {
 
   useEffect(() => {
     window.addEventListener('mousemove', onMouseMove);
+
     return () => {
       window.removeEventListener('mousemove', onMouseMove);
     };
   }, []);
 
   useEffect(() => {
-    // Simulate loading for a few seconds
-    setTimeout(() => {
+    const loadingTimeout = setTimeout(() => {
       setIsLoading(false);
-    }, 3000); // Adjust the time as needed
-  }, []);
+    }, 3000);
 
-  const [isMobile, setIsMobile] = useState(window.matchMedia('(max-width: 767px)').matches);
+    return () => clearTimeout(loadingTimeout);
+  }, []);
 
   useEffect(() => {
     const mediaQuery = window.matchMedia('(max-width: 767px)');
+
     const handleMediaQueryChange = (e) => {
       setIsMobile(e.matches);
     };
 
-    mediaQuery.addListener(handleMediaQueryChange);
-
-    // Initial check for screen size
     setIsMobile(mediaQuery.matches);
 
-    return () => {
-      // Clean up the listener on unmount
-      mediaQuery.removeListener(handleMediaQueryChange);
-    };
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => mediaQuery.removeEventListener('change', handleMediaQueryChange);
   }, []);
 
   return (
--- src/src/mobile/index.js
+++ src/src/mobile/index.js
@@ -4,10 +4,10 @@ import { Route, Routes as RouterRoutes } from 'react-router-dom';
 import Home from './pages/home';
 import Header from './components/others/Header'
 import Footer from './components/others/Footer';
-// import SubscribePopup from './components/others/SubscribePopup';
+
 // import Careers from './pages/carrers';
 // import Login from './pages/login';
-// import Contact from './pages/contact';
+import Contact from './pages/contact';
 
 const MobileRoutes = () => {
   return (
@@ -15,13 +15,12 @@ const MobileRoutes = () => {
       <Route exact path="/" element={<>
       <Header />
       <Home /> 
-      {/* <SubscribePopup /> */}
       <Footer />
       </>} />
      
       
-      {/* <Route  path="/Careers" element={<><Header /><Careers /><Footer /></>} />
-      <Route  path="/contact" element={<><Header /><Contact /><Footer /></>} /> */}
+      {/* <Route  path="/Careers" element={<><Header /><Careers /><Footer /></>} /> */}
+      <Route  path="/contact" element={<><Header /><Contact /><Footer /></>} />
       <Route exact path="*" element={<Home />} />
     </RouterRoutes>
   );
