ccb1808a15f3ae3efbb3d2f56040469fced67c47
thowfick official <mdthowfickspeed@gmail.com>
2023-12-14 19:02:21 +0530

    puplic dic update

--- src/src/desktop/components/SubProSer/SubProSerComponent/SubProSerDetails/index.js
+++ src/src/desktop/components/SubProSer/SubProSerComponent/SubProSerDetails/index.js
@@ -1,51 +1,76 @@
 // SubProSerDetails.js
 
-import React from 'react';
-import SubProSerComponentContent from '../../../../../../data/SubProSer/SubProSerData';
+import React from "react";
+import SubProSerComponentContent from "../../../../../../data/SubProSer/SubProSerData";
 import "./SubProSerDetails.css";
 
 const SubProSerDetails = () => {
   return (
-    <div className='SubProSerDetails-main'>
-      <div className='SubProSerDetailsContentIntro-container'>
-        <h1 className='SubProSerDetailsContentIntro-heading'>{SubProSerComponentContent.SubProSerDetailsIntro.heading}</h1>
-        <p className='SubProSerDetailsContentIntro-paragraph SubProSer-details-text'>{SubProSerComponentContent.SubProSerDetailsIntro.paragraph}</p>
+    <div className="SubProSerDetails-main">
+      <div className="SubProSerDetailsContentIntro-container">
+        <h1 className="SubProSerDetailsContentIntro-heading">
+          {SubProSerComponentContent.SubProSerDetailsIntro.heading}
+        </h1>
+        <p className="SubProSerDetailsContentIntro-paragraph SubProSer-details-text">
+          {SubProSerComponentContent.SubProSerDetailsIntro.paragraph}
+        </p>
       </div>
-      {SubProSerComponentContent.SubProSerDetailsContent.map((content, index) => (
-        <div className={`SubProSer-details-row-fluid ${index % 2 === 0 ? 'SubProSer-details-odd' : 'SubProSer-details-even'}`} key={index}>
-          {index % 2 === 0 ? (
-            <>
-              <div className="SubProSer-details-col">
-                <div className="SubProSer-details-image-col">
-                  <img src={content.imageUrl} alt={content.imageAlt} />
+      {SubProSerComponentContent.SubProSerDetailsContent.map(
+        (content, index) => (
+          <div
+            className={`SubProSer-details-row-fluid ${
+              index % 2 === 0
+                ? "SubProSer-details-odd"
+                : "SubProSer-details-even"
+            }`}
+            key={index}
+          >
+            {index % 2 === 0 ? (
+              <>
+                <div className="SubProSer-details-col">
+                  <div className="SubProSer-details-image-col">
+                    <img src={content.imageUrl} alt={content.imageAlt} />
+                  </div>
                 </div>
-              </div>
-              <div className="SubProSer-details-col">
-                <div className="SubProSer-details-text-col">
-                  <p className='SubProSer-details-paragraph SubProSer-details-text'>{content.title}</p>
-                  <h1 className='SubProSer-details-text'>{content.heading}</h1>
-                  <p className='SubProSer-details-paragraph SubProSer-details-text'>{content.paragraph}</p>
+                <div className="SubProSer-details-col">
+                  <div className="SubProSer-details-text-col">
+                    <p className="SubProSer-details-paragraph SubProSer-details-text">
+                      {content.title}
+                    </p>
+                    <h1 className="SubProSer-details-text">
+                      {content.heading}
+                    </h1>
+                    <p className="SubProSer-details-paragraph SubProSer-details-text">
+                      {content.paragraph}
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </>
-          ) : (
-            <>
-              <div className="SubProSer-details-col">
-                <div className="SubProSer-details-text-col">
-                  <p className='SubProSer-details-paragraph SubProSer-details-text'>{content.title}</p>
-                  <h1 className='SubProSer-details-text'>{content.heading}</h1>
-                  <p className='SubProSer-details-paragraph SubProSer-details-text'>{content.paragraph}</p>
+              </>
+            ) : (
+              <>
+                <div className="SubProSer-details-col">
+                  <div className="SubProSer-details-text-col">
+                    <p className="SubProSer-details-paragraph SubProSer-details-text">
+                      {content.title}
+                    </p>
+                    <h1 className="SubProSer-details-text">
+                      {content.heading}
+                    </h1>
+                    <p className="SubProSer-details-paragraph SubProSer-details-text">
+                      {content.paragraph}
+                    </p>
+                  </div>
                 </div>
-              </div>
-              <div className="SubProSer-details-col  ">
-                <div className="SubProSer-details-image-col">
-                  <img src={content.imageUrl} alt={content.imageAlt} />
+                <div className="SubProSer-details-col  ">
+                  <div className="SubProSer-details-image-col">
+                    <img src={content.imageUrl} alt={content.imageAlt} />
+                  </div>
                 </div>
-              </div>
-            </>
-          )}
-        </div>
-      ))}
+              </>
+            )}
+          </div>
+        )
+      )}
     </div>
   );
 };
